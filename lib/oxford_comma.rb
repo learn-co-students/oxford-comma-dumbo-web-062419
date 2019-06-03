48ed07efc50bab9b6c032cb39739db819cb9fd54
Olga Rosas <olga.rosas@gmail.com>
2019-06-03 17:37:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
 def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
+    i = (array.length-2)
     last = " and " + array[-1]
-    array[0..1].join(", ") + last
-  elsif array.length >= 4
-    array[0..[-2]].join(", ") + last
+    array[0..i].join(", ") + last
   else
     array.join
-end
+  end
 end
