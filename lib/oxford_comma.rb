ebaf1d8d9a0c7ae49d69821969f97388f03f9fd2
Olga Rosas <olga.rosas@gmail.com>
2019-06-03 17:04:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   elsif array.length == 3
-
+    array[1..2].join(", ") + array.join(" and ")
   elsif array.length >= 4
 
   else
