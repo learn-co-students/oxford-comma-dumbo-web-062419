e2d4b25e87b7b621476629a0a11cad7cd965a140
JahazielGuzman <jahaziel.guzman93@gmail.com>
2019-06-11 13:00:39 -0400

    completed oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+	if array.length == 2
+		array.join " and "
+	elsif array.length >= 3
+		last_elem = array[-1]
+		s = array[0...-1].join ", "
+		s.concat", and #{last_elem}"
+	else
+		array.join
+	end
 end
